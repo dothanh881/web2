befcaaf01767ac06ee2a1e7d3a70dce5a7f70b4c
thanhdo881 <thanhdp881@gmail.com>
2024-05-12 18:20:37 +0700

    up

--- Template/_banner-area.php
+++ Template/_banner-area.php
@@ -3,10 +3,10 @@
 <section id="banner-area">
     <div class="owl-carousel owl-theme">
         <!-- <div class="item">
-            <img src="./assets/bannerNew4.png" alt="Banner1">
+            <img src="./assets/bannerNew8.jpg" alt="Banner1">
         </div> -->
         <div class="item">
-            <img src="./assets/bannerNew5.png" alt="Banner2">
+            <img src="./assets/bannerNew7.jpg" alt="Banner2">
         </div>
         <div class="item">
             <img src="./assets/bannerNew6.png" alt="Banner3">
